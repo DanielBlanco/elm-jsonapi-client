fb1c4789fc5f63bc7ba3731cf75557932cb703c5
Daniel Blanco <daniel.blancorojas@gmail.com>
2018-03-12 17:31:17 -0600

    Improve the header

--- src/View/Header.elm
+++ src/View/Header.elm
@@ -6,5 +6,20 @@ import Html.Attributes exposing (..)
 
 view : Html msg
 view =
+    section [ class "hero is-primary" ]
+        [ div
+            [ class "hero-body" ]
+            [ imgLogo, title ]
+        ]
 
-    img [ src "/logo.svg" ] []
+
+imgLogo : Html msg
+imgLogo =
+    img [ class "logo", src "/logo.svg" ] []
+
+
+title : Html msg
+title =
+    h5
+        [ class "is-uppercase has-text-weight-bold" ]
+        [ text "User List" ]
